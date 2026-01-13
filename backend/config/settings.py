de1c377d011478ab6c1ca7ad22357dfd29c996b6
Development Team <dev@versity.local>
2026-01-13 12:04:54 +0600

    fix: Add 0.0.0.0 to ALLOWED_HOSTS for local development

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-cj-v)twa&w9z81u%&n8#v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0', cast=lambda v: [s.strip() for s in v.split(',')])
 DOMAIN = config('DOMAIN', default='localhost:8000')
 
 
